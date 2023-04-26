ff8c4d65d7c0d5b48a6305a1659d58787bb7df09
Ali Gholipour <aligholipourkhalili@gmail.com>
2023-04-26 12:00:16 +0430

    Added AppDbContext for save changes in database

---
+++ StatePatternEFCore/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StatePatternEFCore.Domains.Order;
+
+namespace StatePatternEFCore.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> option) : base(option)
+        {
+        }
+
+        public DbSet<Order> Order { get; set; }
+    }
+}
