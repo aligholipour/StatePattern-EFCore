0ee7d1675d222974e7c4a473ac33c4715670320b
Ali Gholipour <aligholipourkhalili@gmail.com>
2023-04-26 12:03:36 +0430

    Added StateConverter for serialize and deserialize OrderState in database

---
+++ StatePatternEFCore/Data/StateConverter.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using StatePatternEFCore.Domains.Order;
+
+namespace StatePatternEFCore.Data
+{
+    public class StateConverter : ValueConverter<OrderState, string>
+    {
+        private static Dictionary<string, Type> _stateTypeMap;
+
+        public StateConverter(IEnumerable<Type> stateTypes) : base(
+            v => Serialize(v),
+            v => Deserialize(v))
+        {
+            _stateTypeMap = stateTypes.ToDictionary(
+               t => t.Name,
+               t => t);
+        }
+
+        private static string Serialize(OrderState state)
+        {
+            if (state == null) return null;
+            return state.GetType().Name;
+        }
+
+        private static OrderState Deserialize(string value)
+        {
+            if (value == null) return null;
+
+            return !_stateTypeMap.TryGetValue(value, out Type stateType)
+                ? throw new ArgumentException($"Unknown state type '{value}'")
+                : Activator.CreateInstance(stateType) as OrderState;
+        }
+    }
+}
