7883d85b39cea510934506350a8d52cb1fe3f748
Ali Gholipour <aligholipourkhalili@gmail.com>
2023-04-26 12:01:59 +0430

    Added ReflectionHelper for find OrderStatus subclasses

---
+++ StatePatternEFCore/Helpers/ReflectionHelper.cs
@@ -0,0 +1,14 @@
+﻿using StatePatternEFCore.Domains.Order;
+
+namespace StatePatternEFCore.Helpers
+{
+    public static class ReflectionHelper
+    {
+        public static Type[] FindOrderStateSubclasses()
+        {
+            var assembly = typeof(OrderState).Assembly;
+            var types = assembly.GetTypes();
+            return types.Where(t => t.IsSubclassOf(typeof(OrderState))).ToArray();
+        }
+    }
+}
